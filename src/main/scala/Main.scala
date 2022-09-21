820e684acb18b507aff1accdc15a6dcd00c8153d
Katherine <katherine-fu@outlook.com>
2022-09-21 12:03:54 +0200

    commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,16 @@
+package zioplayground
+
 import zio._
 import java.io.IOException
 import scala.io.AnsiColor._
 
-object ourConsole {
-  def printLine(line: String) = {
-    ZIO.succeed(println(line))
-  }
-
-  val readLine =
-    ZIO.succeed(scala.io.StdIn.readLine)
-}
-
 object Main extends scala.App {
+  Runtime.default.unsafeRunSync(program)
+
   val program =
     for (
-      _ <- ourConsole.printLine("What is your name?");
-      name <- ourConsole.readLine;
-      _ <- ourConsole.printLine(s"Hello $name!")
+      _ <- Console.printLine("What is your name?");
+      name <- Console.readLine;
+      _ <- Console.printLine(s"Hello $name!")
     ) yield ()
-
-  val runtime: Runtime[Any] = Runtime.default
-
-  Unsafe.unsafe { implicit unsafe =>
-    runtime.unsafe
-      .run(program)
-      .getOrThrowFiberFailure()
-  }
 }
