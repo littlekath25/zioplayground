379927d98bc30b1b09d167c6c0bf9d68942a6fd3
Katherine <katherine-fu@outlook.com>
2022-09-26 11:02:25 +0200

    commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,46 @@
 package zioplayground
 
 import zio._
-import java.io.IOException
-import scala.io.AnsiColor._
 
-object Main extends scala.App {
-  Runtime.default.unsafeRunSync(program)
+object MyApp {
+  final case class Callable[+S](execute: () => S) { self => 
+    def orElse[S1 >: S](that: Callable[S1]): Callable[S1] =
+      Callable(() => try self.execute() catch { case _ : Throwable => that.execute()}) 
 
-  val program =
-    for (
-      _ <- Console.printLine("What is your name?");
-      name <- Console.readLine;
-      _ <- Console.printLine(s"Hello $name!")
-    ) yield ()
+    def orElseEither[S2](that: Callable[S2]): Callable[Either[S, S2]] = ???
+
+    def andThen[S2](onSuccess: S => Callable[S2]): Callable[S2] = ???
+  }
+
+  val hello = Callable(() => println("hello"))
+  val bye = Callable(() => println("bye"))
+
+  val askName = Callable(() => println("What is your name?"))
+  val getName = Callable(() => scala.io.StdIn.readLine())
+  def printName(name: String): Callable[Unit] = Callable(() => println(s"Hello ${name})"))
+
+  val helloOrBye = hello.orElse(bye)
+
+  def callable[S](s: => S) = Callable(() => s)
+
+  def retry[S](statements: Callable[S], n: Int): Callable[S] = ???
+
+  def parallelize[S](statements: Callable[S]): Callable[S] = ???
+
+  def fourth(x: Double) = {
+    val x_squared = x * x
+
+    x_squared * x_squared
+  }
+
+  /* 
+  1. Extract variable
+  2. Inline variable
+  3. Extract method
+  4. Inline method
+  */
+
+  final case class Rule[S](condition: () => Boolean, statements: Callable[S])
+
+  def main(args: Array[String]): Unit = helloOrBye.execute()
 }
